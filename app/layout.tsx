77fb29ab9f2676d1587772cc27701a46b9c100bc
anthonyeft <aefthimi8@gmail.com>
2025-03-09 21:24:18 -0400

    clean unnecessary styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,32 +4,23 @@ import "./globals.css";
 import { ThemeProvider } from "./provider";
 import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Anthony Efthimiadis",
   description: "Portfolio and contact of Anthony Efthimiadis",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </head>
       <body className={inter.className}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-            <Toaster position="top-right" />
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          {children}
+          <Toaster position="top-right" />
         </ThemeProvider>
       </body>
     </html>
